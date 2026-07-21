6fda2f20255cefd6589e1281d588b0da1a3da7d2
yuki_yano <yuki.ycino@gmail.com>
2026-07-21 14:38:00 +0900

    Stabilize config stop race test

--- Tests/ShitsuraeCoreTests/ConfigWatcherTests.swift
+++ Tests/ShitsuraeCoreTests/ConfigWatcherTests.swift
@@ -99,7 +99,7 @@ struct ConfigWatcherTests {
                 group.leave()
             }
 
-            #expect(group.wait(timeout: .now() + 1) == .success)
+            #expect(group.wait(timeout: .now() + 5) == .success)
             manager.stop()
         }
     }
